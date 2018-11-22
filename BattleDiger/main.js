dc39ee45eadcb9f676c08f6e12908e3e137dbdb8
RoundCat0857 <round0857cat@gmail.com>
2018-11-22 18:59:53 +0900

    return circle only for 5n floor

--- BattleDiger/main.js
+++ BattleDiger/main.js
@@ -1,13 +1,19 @@
 enchant()
 
+let floor = 0
+
 window.onload = function load() {
   const core = new Core(256, 192)
   core.fps = 10
   core.preload('./texture/map1.png', './texture/char1.png', './texture/back.png', './texture/effect1.png')
   core.onload = function() {
-    const { field, object, collision, start, next } = maps[2]
+    const { field, object, collision, start, next, back } = maps[2]
     const obj = JSON.parse(JSON.stringify(object))
     const col = JSON.parse(JSON.stringify(collision))
+    if (floor % 5 === 4) {
+      obj[back[1]][back[0]] = 29
+    }
+    floor += 1
 
     const map = new Map(16,16)
     map.image = core.assets['./texture/map1.png']
@@ -21,15 +27,14 @@ window.onload = function load() {
     const player = new Sprite(48,48)
     player.image = core.assets['./texture/char1.png']
     player.scale(0.33,0.33)
-    const [x,y] = start
-    player.x = x * 16
-    player.y = y * 16
+    player.x = (start[0]-1) * 16
+    player.y = (start[1]-1) * 16
 
     player.isMoving = false
     // player.direction = 0
     // player.walk = 1
     player.addEventListener('enterframe', async function() {
-      if (this.x === next[0] * 16 && this.y === next[1] * 16) {
+      if (this.x === (next[0]-1) * 16 && this.y === (next[1]-1) * 16) {
         load()
       }
       // this.frame = this.direction * 3 + this.walk
